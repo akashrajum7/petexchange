56357f592e07f3dbcfb74efb34f6a5117e8110fc
Akash Raju M <akashrajum7@gmail.com>
2019-07-13 13:00:27 +0530

    Adding homepage with grid

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ app.set("view engine", "ejs");
 //Routes
 
 app.get("/",function(req,res){
-    res.render("homepage");
+    var petsforsale = [
+        {title:"Bull Dog",image:"https://negativespace.co/wp-content/uploads/2018/01/negative-space-bulldog-dog-pet-sleep-couch-thumb-1.jpg",location:"Bangalore",seller:"Chandan",price:"20000"},
+        {title:"Labrador",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Chennai",seller:"Vamsi",price:"10000"},
+        {title:"Poodle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Delhi",seller:"Abhishek",price:"30000"},
+        {title:"Pug",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Mumbai",seller:"Arun",price:"15000"},
+        {title:"Beagle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Goa",seller:"Akash",price:"25000"},
+        {title:"Beagle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Goa",seller:"Akash",price:"25000"},
+        {title:"Beagle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Goa",seller:"Akash",price:"25000"},
+        {title:"Beagle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Goa",seller:"Akash",price:"25000"},
+        {title:"Beagle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Goa",seller:"Akash",price:"25000"},
+        {title:"Beagle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Goa",seller:"Akash",price:"25000"},
+        {title:"Beagle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Goa",seller:"Akash",price:"25000"},
+        {title:"Beagle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Goa",seller:"Akash",price:"25000"},
+        {title:"Beagle",image:"https://negativespace.co/wp-content/uploads/2018/08/negative-space-golden-labrador-puppies-dog-happy-field-grass-flowers-chevanon-photography-thumb-1.jpg",location:"Goa",seller:"Akash",price:"25000"},
+    ]
+    res.render("homepage",{petsforsale:petsforsale});
 });
 
 //Port to listen on
