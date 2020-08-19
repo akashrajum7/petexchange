272725affea278462d70e9917b521bf27da37c67
akashrajum7 <akashrajum7@gmail.com>
2020-08-19 11:58:08 +0530

    Fix description

--- models/pet.js
+++ models/pet.js
@@ -1,36 +1,38 @@
-const   mongoose = require("mongoose"),
-        User     = require("./user");
-        
+const mongoose = require("mongoose"),
+  User = require("./user");
+
 //Pet schema
 var petSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
+  title: {
+    type: String,
+    required: true,
+  },
+  imageurl: {
+    type: String,
+    required: true,
+  },
+  discription: String,
+  category: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  user: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-    imageurl: {
-        type: String,
-        required: true
-    },
-    description: String,
-    category: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required:true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    user: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }]
+  ],
 });
 
-var Pet = mongoose.model("Pet",petSchema);
+var Pet = mongoose.model("Pet", petSchema);
 
-//Exporting pet model 
+//Exporting pet model
 module.exports = Pet;
